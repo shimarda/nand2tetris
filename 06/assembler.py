2148fedcd7209ac95bb57347288044a1c9661ec4
shimarda <ryo1031shima@gmail.com>
2025-08-31 12:39:11 +0900

    computer.hdlを追加

--- 06/assembler.py
+++ 06/assembler.py
@@ -1,66 +1,73 @@
 import re
 
-class Parser():
-    def init():
-        return None
+class Parser:
+    # 入力ファイル/データストリームを開き解析の準備をする
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.fp = open(file_path, "r")
+        
 
     # 入力にまだ行があるのか
-    def hasMoreLines():
-        return None
+    def hasMoreLines(self) -> bool:
+        cur_line = self.fp.tell()
+        line = self.fp.readline()
+        self.fp.seek(cur_line)
+        return line != ''
 
     # 空白、コメントをスキップ
-    def advance():
+    def advance(self):
+        
         return None
 
     # 命令のタイプを示す
-    def instructionType():
+    def instructionType(self):
         return None
 
     # 命令が(〇〇)の場合シンボル〇〇を返す
-    def symbol():
+    def symbol(self):
         return None
 
     # 現在のC命令のdest部分を返す
-    def dest():
+    def dest(self):
         return None
 
     # 現在のC命令のcomp部分を返す
-    def comp():
+    def comp(self):
         return None
 
     # 現在のC命令のjump部分を返す
-    def jump():
+    def jump(self):
         return None
 
-class Code():
+class Code:
     # destニーモニックのバイナリコード
-    def dest():
+    def dest(self):
         return None
     
     # compニーモニックのバイナリコード
-    def comp():
+    def comp(self):
         return None
 
     # jumpニーモニックのバイナリコード
-    def jump():
+    def jump(selg):
         return None
 
 
-class SymbolTable():
+class SymbolTable:
     # 新しい空のシンボルテーブルを作成
-    def init():
+    def init(self):
         return None
 
     # <symbot, address>をテーブルに追加
-    def addEntry():
+    def addEntry(self):
         return None
 
     # 指定されたsymbolがテーブルに含まれているのか
-    def contains():
+    def contains(self):
         return None
 
     # symbolに関連付けられたアドレスを返す
-    def getAddress():
+    def getAddress(self):
         return None
 
 
