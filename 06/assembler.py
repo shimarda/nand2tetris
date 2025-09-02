5b946a6e687791a9a2b7f62960dc15fcedc478be
shimarda <ryo1031shima@gmail.com>
2025-09-02 09:31:20 +0900

    nr

--- 06/assembler.py
+++ 06/assembler.py
@@ -39,11 +39,11 @@ class Parser:
         if self.instruction_type:
             return self.instruction_type
         
-        line = self.fp.readline()
+        line = self.current_line
         if line.startswith('@'):
             self.instruction_type = A_INSTRUCTION
             return A_INSTRUCTION
-        elif re.fullmatch(r'\(*\)', line):
+        elif re.fullmatch(r'\(.+\)', line):
             self.instruction_type = L_INSTRUCTION
             return L_INSTRUCTION
         else:
@@ -55,10 +55,10 @@ class Parser:
         line = self.current_line
         instr_type = self.instructionType()
         if instr_type == L_INSTRUCTION:
-            m = re.search(r'\((*)\)', line)
+            m = re.search(r'\((.+)\)', line)
             return m.group(1)
         elif instr_type == A_INSTRUCTION:
-            m = re.search(r'@(*)', line)
+            m = re.search(r'@(.+)', line)
             return m.group(1)
         return None
 
@@ -81,8 +81,9 @@ class Parser:
     # 現在のC命令のjump部分を返す
     def jump(self):
         line = self.current_line
-        return line.split(';')[1].strip()
-    
+        if ';' in line:
+            return line.split(';')[1].strip()
+        return None
     def close(self):
         self.fp.close()
 
@@ -93,12 +94,13 @@ class Code:
     # destニーモニックのバイナリコード
     def dest(self, dest):
         res = ['0', '0', '0']
-        if "M" in dest:
-            res[2] = '1'
-        if "D" in dest:
-            res[1] = '1'
-        if "A" in dest:
-            res[0] = '1'
+        if dest:
+            if "M" in dest:
+                res[2] = '1'
+            if "D" in dest:
+                res[1] = '1'
+            if "A" in dest:
+                res[0] = '1'
         return ''.join(res)
     
     # compニーモニックのバイナリコード
@@ -154,49 +156,105 @@ class Code:
 class SymbolTable:
     # 新しい空のシンボルテーブルを作成
     def __init__(self):
-        
-        return None
+        self.table = {
+            "R0": 0, "R1": 1, "R2": 2, "R3": 3, "R4": 4, "R5": 5, "R6": 6, "R7": 7, "R8": 8, "R9": 9, "R10": 10, "R11": 11, "R12": 12, "R13": 13, "R14": 14, "R15": 15,
+            "SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4, "SCREEN": 16384, "KBD": 24576
+        }        
 
     # <symbot, address>をテーブルに追加
-    def addEntry(self):
-        return None
+    def addEntry(self, symbol, address):
+        self.table[symbol] = address
 
     # 指定されたsymbolがテーブルに含まれているのか
-    def contains(self):
-        return None
+    def contains(self, symbol):
+        return symbol in self.table
 
     # symbolに関連付けられたアドレスを返す
-    def getAddress(self):
-        return None
+    def getAddress(self, symbol):
+        return self.table.get(symbol)
 
+def assemble_file(input_file, output_file):
+    parser = Parser(input_file)
+    code = Code()
+    symbol_table = SymbolTable()
 
+    # 第一パス
+    rom_addr = 0
+    while parser.hasMoreLines():
+        line = parser.advance()
+        if line is None:
+            break
 
+        instr_type = parser.instructionType()
+        if instr_type == L_INSTRUCTION:
+            symbol = parser.symbol()
+            symbol_table.addEntry(symbol, rom_addr)
+        else:
+            rom_addr += 1
 
+    parser.close()
+
+    # 第二パス
+    parser = Parser(input_file)
+    ram_addr = 16
+    machine_code = []
+    
+    while parser.hasMoreLines():
+        line = parser.advance()
+        if line is None:
+            break
+
+        instr_type = parser.instructionType()
+
+        if instr_type == A_INSTRUCTION:
+            symbol = parser.symbol()
+
+            if symbol.isdigit():
+                addr = int(symbol)
+            else:
+                if symbol_table.contains(symbol):
+                    addr = symbol_table.getAddress(symbol)
+                else:
+                    symbol_table.addEntry(symbol, ram_addr)
+                    addr = ram_addr
+                    ram_addr += 1
+
+            binary = format(addr, '016b')
+            machine_code.append(binary)
+        
+        elif instr_type == C_INSTRUCTION:
+            dest_str = parser.dest()
+            comp_str = parser.comp()
+            jump_str = parser.jump()
+
+            dest_bits = code.dest(dest_str)
+            comp_bits = code.comp(comp_str)
+            jump_bits = code.jump(jump_str)
+
+            binary = "111" + comp_bits + dest_bits + jump_bits
+            machine_code.append(binary)
+
+    parser.close()
+
+    with open(output_file, 'w') as f:
+        for code_line in machine_code:
+            f.write(code_line + '\n')
 
 if __name__ == "__main__":
     
     args = sys.argv
-    if 1 != len(args):
+    if len(args) != 2:
         print("Argument is invalid")
-    else:
-        parser = Parser(args[0])
-
-        if parser.hasMoreLines():
-            # 次に行あり
-            parser.advance()
-            
-            instr_type = parser.instructionType()
-            if instr_type == A_INSTRUCTION or instr_type == L_INSTRUCTION:
-                symbol = parser.symbol()
-            else:
-                dest = parser.dest()
-                comp = parser.comp()
-                jump = parser.jump()
-
-        else:
-            parser.fp.close()
-
     
+    input_file = args[1]
+    output_file = input_file.replace('.asm', '.hack')
+    
+    try:
+        assemble_file(input_file, output_file)
+        print("アセンブル終了")
+    except Exception as e:
+        print(f"エラー: {e}")
+        sys.exit(1)
 
 
 
