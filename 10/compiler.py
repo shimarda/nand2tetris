7e31f32a55556d69130e2792d1b5773d5d439b58
shimarda <ryo1031shima@gmail.com>
2025-10-13 21:36:30 +0900

    第9章終了、第10章開始

---
+++ 10/compiler.py
@@ -0,0 +1,12 @@
+class JackTokenizer:
+    def __init__(self):
+        self.file = ""
+
+
+class CompilationEngine:
+    def __init__(self):
+        self.file = ""
+
+class JackAnalyzer:
+    def __init__(self):
+        self.file = ""
