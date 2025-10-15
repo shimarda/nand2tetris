a5d00115904b7b2dc845b6a03cf76e68e700846c
shimarda <ryo1031shima@gmail.com>
2025-10-15 13:50:45 +0900

    トークナイズ部分を修正中

--- 10/compiler.py
+++ 10/compiler.py
@@ -40,44 +40,104 @@ class JackTokenizer:
     def __init__(self, file_path: str) -> None:
         self.file = file_path
         self.fp = open(file_path, "r")
-        self.cur_token = ""
         self.cur_line = ""
-        self.token_type = None
+        self.cur_position = 0
+        self.cur_token = ""
+        self.token_lst = list()
+        self.cur_token_type = None
 
     def hasMoreTokens(self) -> bool:
-        before, separator, after = self.cur_line.partition(self.cur_token)
-        if after is "":
+        length = len(self.token_lst)
+        if int(length) - 1 == self.cur_position:
             return False
-        else:
+        else :
             return True
-        
-    def tokenizeLine(self):
+    
+    def tokenizeLines(self):
+        for line in self.fp:
+            if line.startswith("//") or line.startswith('/*'):
+                continue
+            self.cur_line = line
+            self.tokenizeLine()
 
-            
+    # 1行をトークンへ
+    def tokenizeLine(self):
+        line = self.cur_line
+        token = ""
+        # インデックス i を使って文字列を走査
+        i = 0
+        while i < len(line):
+            char = line[i]
+
+            if char == '\n':
+                if token:
+                    self.token_lst.append(token)
+                i = len(line)
+
+            elif char in symbol_lst:
+                if token:
+                    self.token_lst.append(token)
+                self.token_lst.append(char)
+                token = ""
+                i += 1
+
+            elif char == ' ':
+                if token:
+                    self.token_lst.append(token)
+                    token = ""
+                i += 1
+                
+            elif char == '"':
+                if token:
+                    self.token_lst.append(token)
+                
+                self.token_lst.append('"')
+                
+                try:
+                    next_quote_index = line.index('"', i + 1)
+                    
+                    string_content = line[i + 1 : next_quote_index]
+                    self.token_lst.append(string_content)
+                    
+                    self.token_lst.append('"')
+                    
+                    i = next_quote_index + 1
+                    
+                except ValueError:
+                    string_content = line[i + 1:]
+                    self.token_lst.append(string_content)
+                    i = len(line) # ループを終了
+                
+                token = "" # トークンをリセット
+
+            else :
+                token += char
+                i += 1
+        
+        # 行の終わりに残っているトークンを処理
+        if token:
+            self.token_lst.append(token)
+
+    # 次のトークンを取得
     def advance(self):
-        self.cur_line = self.fp.readline()
-        line_lst = self.cur_line.strip()
-        try:
-            cur_num = line_lst.index(self.cur_token)
-            self.cur_token = line_lst[cur_num+1]
-        except ValueError:
-            self.cur_token = line_lst[0]
+        self.cur_position += 1
+        self.cur_token = self.token_lst[self.cur_position]
 
     def tokenType(self):
         token = self.cur_token
 
         if token in keyword_lst:
-            self.token_type = KEYWORD
+            self.cur_token_type = KEYWORD
         elif token in symbol_lst:
-            self.token_type = SYMBOL
+            self.cur_token_type = SYMBOL
         elif 0 <= int(token) <= 32767:
-            self.token_type = INT_CONST
+            self.cur_token_type = INT_CONST
         elif '"' not in token and '\n' not in token:
-            self.token_type = STRING_CONST
+            self.cur_token_type = STRING_CONST
         else:
-            self.token_type = IDENTIFIER
+            self.cur_token_type = IDENTIFIER
         
-        return self.token_type
+        return self.cur_token_type
     
     def KeyWord(self):
         if self.cur_token == "class":
@@ -144,21 +204,21 @@ class CompilationEngine:
 
     def compileClass(self) -> None:
 
-        if (self.input_file.token_type == CLASS):
+        if (self.input_file.cur_token_type == CLASS):
             self.fp_out.write(f"<class> {self.input_file.cur_token} </class>")
 
     def compileClassVarDec(self) -> None:
-        if (self.input_file.token_type == STATIC):
+        if (self.input_file.cur_token_type == STATIC):
             self.fp_out.write(f"<static> {self.input_file.cur_token} </static>")
-        elif (self.input_file.token_type == FIELD):
+        elif (self.input_file.cur_token_type == FIELD):
             self.fp_out.write(f"<field> {self.input_file.cur_token} </field>")
         
     def compileSubroutine(self) -> None:
-        if (self.input_file.token_type == METHOD):
+        if (self.input_file.cur_token_type == METHOD):
             self.fp_out.write(f"<method> {self.input_file.cur_token} </method>")
-        elif (self.input_file.token_type == FUNCTION):
+        elif (self.input_file.cur_token_type == FUNCTION):
             self.fp_out.write(f"<function> {self.input_file.cur_token} </function>")
-        elif (self.input_file.token_type == CONSTRUCTOR):
+        elif (self.input_file.cur_token_type == CONSTRUCTOR):
             self.fp_out.write(f"<constructor> {self.input_file.cur_token} </constructor>")
     
     def compileParameterList(self) -> None:
